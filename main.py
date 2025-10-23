777d25816ee85304ed3cf30bbdb30f59c50473b4
Larcyn78 <jrasch78@gmail.com>
2025-10-23 17:34:32 -0600

    Added basic Game window

--- main.py
+++ main.py
@@ -2,11 +2,21 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
+        screen.fill((0,0,0), rect=None, special_flags=0)
+        pygame.display.flip()
+    
 if __name__ == "__main__":
     main()
