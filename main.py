d14fe0ae3ffbe764c8b4a53128201e81c5e7e54e
Larcyn78 <jrasch78@gmail.com>
2025-10-23 18:20:48 -0600

    added player and controls

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable,drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         for event in pygame.event.get():
@@ -12,7 +22,14 @@ def main():
                 return
 
         screen.fill("black")
+        for d in drawable:
+            d.draw(screen)
+        
+        updateable.update(dt)
         pygame.display.flip()
+
+        # limit to 60 fps
+        dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
